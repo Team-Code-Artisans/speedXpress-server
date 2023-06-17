69219efac1cc5ec2de27e0fb1e8729d272892a6e
Tofayel <mdtofayelhossain69@gmail.com>
2023-06-17 10:55:01 +0600

    tofayel nothing added

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const { MongoClient, ServerApiVersion } = require("mongodb");
 const jwt = require("jsonwebtoken")
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.n84h1t4.mongodb.net/?retryWrites=true&w=majority`;
+
+console.log(uri);
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
