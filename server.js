74e7dc6b6353cbc4c9c9cb55e7bfb5364a0ebfc5
Tofayel <mdtofayelhossain69@gmail.com>
2023-06-18 09:46:52 +0600

    server bug fix of finding of my coustomer data in database

--- server.js
+++ server.js
@@ -6,7 +6,6 @@ const jwt = require("jsonwebtoken");
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.n84h1t4.mongodb.net/?retryWrites=true&w=majority`;
 
-console.log(uri);
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
@@ -111,11 +110,11 @@ app.get("/user/:email", async (req, res) => {
 // get all customers
 app.get("/customers/:email", async (req, res) => {
   try {
-    const customerOwnerEmail = req.params.email;
-    console.log("owner email", customerOwnerEmail);
+    const merchantEmail = req.params.email;
+    console.log("merchant Email", merchantEmail);
 
     const result = await customerCollection
-      .find({ customerOwnerEmail: customerOwnerEmail })
+      .find({ merchantEmail: merchantEmail })
       .toArray();
     if (result.length) {
       res.status(200).send({
